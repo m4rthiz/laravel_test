c9e8804e8fe6ba1e98bd287594877798771bf41d
marie.thizy <marie.thizy@emse.fr>
2018-04-16 17:26:58 +0200

    add database sqlite

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'fr',
 
     /*
     |--------------------------------------------------------------------------
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -208,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Calendar' => MaddHatter\LaravelFullcalendar\Facades\Calendar::class,
 
     ],
 
