9d1210a6c3e2708e41d83dc4c47c6ddd5f7c6826
marie.thizy <marie.thizy@emse.fr>
2018-05-29 14:21:55 +0200

    ajout cdn pour locale ok

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
-    {{--<script href="{{ asset('js/fullcalendar.js') }}"></script>--}}
-    <script href='{{ asset('js/fr.js') }}'></script>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/moment@2.22.1/moment.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/locale/fr.js"></script>
+
 
 
     <style>
@@ -20,6 +22,7 @@
     </style>
 </head>
 <body>
+
 <div class="container">
 
     <div class="panel panel-primary">
@@ -30,6 +33,7 @@
             {!! $calendar->calendar() !!}
             {!! $calendar->script() !!}
         </div>
+
     </div>
 </div>
 
